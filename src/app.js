f3fdbc09d76d739ca6e8e06f5ae75c29fe1fe7d0
ysz951 <shengyang_zhou@baylor.edu>
2020-07-19 00:20:40 -0500

    optimize code

--- src/app.js
+++ src/app.js
@@ -4,10 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const db = require('./store')
-const notesRouter = require('./notes/notes-router')
-const foldersRouter = require('./folders/folders-router')
 const foldernoteRouter = require('./folder-note/folder-note-router')
+const errorHandler = require('./errorHandler')
+const validateBearerToken = require('./validateBearerToken')
+
 const app = express()
 
 const morganOption = (NODE_ENV === 'production')
@@ -18,17 +18,8 @@ app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.use(function errorHandler(error, req, res, next) {
-    let response
-    if (NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
-    } else {
-        console.error(error)
-    response = { message: error.message, error }
-    }
-    res.status(500).json(response)
-})
-
+app.use(errorHandler)
+app.use(validateBearerToken)
 app.get('/', (req, res) => {
     res.send('Hello, world!')
 })
